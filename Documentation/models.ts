5ca7b6230bf1562bb2f50dc66b63a66bda1b3e3d
Wesley Edwards <westheedwards@gmail.com>
2022-09-12 14:50:43 -0600

    Adding rough draft of UML and models

---
+++ Documentation/models.ts
@@ -0,0 +1,64 @@
+
+export interface User {
+    _id: string;
+    name: string;
+    email: string;
+    password: string;
+    customer: Customer;
+    employee: Employee | null | undefined;
+}
+
+export interface Employee {
+    permissions: Permissions;
+    workHistory: WorkShift[];
+    hourlyWage: number;
+}
+
+export enum Permissions {
+    ADMIN = "ADMIN",
+    WORKER = "WORKER",
+}
+
+export interface WorkShift {
+    hoursWorked: number;
+    date: string;
+    paymentStatus: PaymentStatus;
+}
+
+export enum PaymentStatus {
+    PAID = "PAID",
+    UNPAID = "UNPAID"
+}
+
+export interface Customer {
+    orderHistory: Order[];
+    accountBalance: number;
+}
+
+export interface Order {
+    cart: SelectedFoodItem[];
+    status: PaymentStatus;
+}
+
+export interface SelectedFoodItem {
+    foodItemId: string;
+    quantity: number;
+    foodModificationIds: string[];
+    foodModQuantity: number;
+}
+
+
+export interface FoodItem {
+    _id: string;
+    name: string;
+    price: number;
+    description: string;
+    possibleModifications: string[];
+}
+
+export interface FoodModifications {
+    _id: string;
+    description: string;
+    name: string;
+    price: number;
+}
