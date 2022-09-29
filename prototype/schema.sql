f64ab7d7ed7a19664447f6d2a24d216d2c414077
Stormray73 <94574890+Stormray73@users.noreply.github.com>
2022-09-28 19:32:39 -0600

    It's almost working - need to iron out last bugs

--- prototype/schema.sql
+++ prototype/schema.sql
@@ -1,9 +1,17 @@
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    UserId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     email TEXT UNIQUE NOT NULL,
     name TEXT NOT NULL,
-    password TEXT NOT NULL,
-    permission INTEGER
+    password TEXT NOT NULL
 );
+
+/*DROP TABLE IF EXISTS permissions;
+
+CREATE TABLE permissions(
+    PermissionId INTEGER PRIMARY KEY AUTOINCREMENT,
+    FOREIGN KEY(UserId) REFERENCES users(UserId),
+    PermissionLevel INTEGER NOT NULL
+)
+--Other tables */
--- prototype/tartarus.py
+++ prototype/tartarus.py
@@ -1,8 +1,10 @@
+# from crypt import methods
+import json
 import os
 import sqlite3
 import jwt
 from this import d
-from urllib import request
+from flask import request
 from flask import Flask
 from flask_cors import CORS
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -27,6 +29,80 @@ def demo():
         return f.read()
 
 # Geoffrey work here #####################
+class User:
+    id = 0
+    name = ""
+    email = ""
+    password = ""
+    permissions = 0
+    def __init__(self, name, email, password, permissions = 0):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.permissions = permissions
+    
+    def getId(self):
+        return self.id
+    def getName(self):
+        return self.name
+    def getEmail(self):
+        return self.email
+    def getPermissions(self):
+        return self.permissions
+    def getPassword(self):
+        return self.password
+    def setId(self, id):
+        self.id = id
+
+@app.route('/newuser', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+        result = request.get_json()
+        newUser = User(
+            result['name'],
+            result['email'],
+            generate_password_hash(result['password']),
+            0
+        )
+        if(checkExistingUser(newUser)):
+            return {'error': 'User already exists'}
+        else:
+            addedUser = addUser(newUser)
+            return json.dumps({
+                'userId': addedUser.getId(),
+                'userEmail' : addedUser.getEmail(),
+                'userName': addedUser.getName(),
+                'userPassword': addedUser.getPassword() 
+                })
+    else:
+        return {'response': 'This is in place of the user sign-up form'}
+
+    
+def checkExistingUser(user):
+    db = get_db()
+    cur = db.cursor()
+    emails = cur.execute("select email from users")
+    emails.fetchall()
+    if (user.getEmail() in emails):
+        return True
+    return False
+
+def addUser(user):
+    db = get_db()
+    cur = db.cursor()
+    cur.execute(f"insert into users(email, name, password) values ('{user.getEmail()}', '{user.getName()}', '{user.getPassword()}')")
+    db.commit()
+    return getUserFromEmail(user.getEmail())
+
+def getUserFromEmail(email):
+    db = get_db()
+    cur = db.cursor()
+    output = cur.execute(f"select * from users where email = '{email}'" )
+    data = output.fetchone()
+    # Insert get permissions
+    user = User(data[1], data[2], data[3], 0)
+    user.setId(data[0])
+    return user
 
 ##########################################
 
@@ -46,8 +122,9 @@ def get_db():
 def refreshdb():
     db = get_db()
     with app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
-    return 'Done'
+        script = str(f.read(), 'utf-8')
+        db.executescript(script)
+    return {'response': 'Done'}
 
 @app.route('/auth/token', methods=(['POST']))
 def get_token():
