89032f5be3c89fd498ae17da5dc153551a260151
LandonDoyle7599 <landondoyle7599@gmail.com>
2022-10-16 18:49:45 -0600

    Working on landing page

--- front_end/src/Views/Home.tsx
+++ front_end/src/Views/Home.tsx
@@ -1,10 +1,10 @@
-import { Container, Stack, Typography } from "@mui/material";
+import {Container, Grid, Stack, Typography} from "@mui/material";
 import React, { FC, useState } from "react";
 import { DFHeader } from "../components/DFHeader";
 import { DrinkCard } from "../components/DrinkCard";
 
 export const Home: FC = () => {
-  const [drinks, setDrinks] = useState(["Frappe"]);
+  const [drinks, setDrinks] = useState(["Frappe", "Cappuccino", "Latte", "Mocha", "Flat White", "Vanilla Bean"]);
 
   const addDrink = (newDrink: string) => {
     setDrinks([...drinks, newDrink]);
@@ -13,12 +13,13 @@ export const Home: FC = () => {
   return (
     <Container maxWidth="md">
       <Typography>There are {drinks.length}</Typography>
-      <Stack gap="8rem" justifyContent="center">
-        <DFHeader title="Welcome to Dan's Frappuccino Hell." />
-        <Typography align="center">Frappuccinos to Come</Typography>
-        {drinks.map((drink) => {
-          return <DrinkCard drink={drink} addDrink={addDrink}/>;
-        })}
+      <Stack gap="2rem" justifyContent="center">
+        <DFHeader title="Welcome to Dan's Frappuccino Hell" />
+          <Grid container rowSpacing={4} columnSpacing={{md:8}}>
+                {drinks.map((drink) => {
+                    return <Grid item md={6}><DrinkCard drink={drink} description={"This is a description"} addDrink={addDrink}/></Grid>;
+                })}
+          </Grid>
       </Stack>
     </Container>
   );
--- front_end/src/components/DrinkCard.tsx
+++ front_end/src/components/DrinkCard.tsx
@@ -6,7 +6,7 @@ import {
   DialogActions,
   DialogContent,
   DialogContentText,
-  DialogTitle,
+  DialogTitle, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent,
   TextField,
   Typography,
 } from "@mui/material";
@@ -14,11 +14,13 @@ import { FC, useState } from "react";
 
 interface DrinkCardProps {
   drink: string;
+  description: string;
   addDrink: (newDrink: string) => void;
 }
 
 export const DrinkCard: FC<DrinkCardProps> = (props) => {
-  const { drink, addDrink } = props;
+  const { drink, description, addDrink } = props;
+
   const [open, setOpen] = useState(false);
   const [newDrink, setNewDrink] = useState("");
 
@@ -30,10 +32,18 @@ export const DrinkCard: FC<DrinkCardProps> = (props) => {
     setOpen(false);
   };
 
-  const handleAddNewDrink = () => {
+  const[ingredients, setIngredients] = useState(["Ingredient1", "Ingredient2", "Ingredient3","Ingredient4",
+  "Ingredient5", "Ingredient6", "Ingredient7", "Ingredient8","Ingredient9","Ingredient10","Ingredient11","Ingredient12"]);
+
+  const handleAddToCart = () => {
     handleClose()
     addDrink(newDrink);
   };
+
+    const [ingredient, setIngredient] = useState('');
+    const handleChange = (event: SelectChangeEvent) => {
+      setIngredient(event.target.value as string);
+    }
 
   return (
     <>
@@ -41,31 +51,47 @@ export const DrinkCard: FC<DrinkCardProps> = (props) => {
         onClick={handleClick}
         style={{
           cursor: "pointer",
+          minWidth: "200px",
+          minHeight: "200px",
         }}
       >
-        <CardContent>
-          <Typography>{drink}</Typography>
+        <CardContent style={{justifyContent: "center"}}>
+          <Typography variant="h5" gutterBottom>{drink}</Typography>
+          <Typography variant="subtitle1" >{description}</Typography>
         </CardContent>
       </Card>
-
-        <DialogTitle>{drink}</DialogTitle>
       <Dialog open={open} onClose={handleClose}>
-        <DialogContent>
-          <DialogContentText>Customize your drink here.</DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            label="My new drink"
-            type="email"
-            fullWidth
-            variant="standard"
-            value={newDrink}
-            onChange={(e) => setNewDrink(e.target.value)}
-          />
+        <DialogContent style={{width: 500}}>
+          <DialogContentText variant="h4" style={{paddingBottom: 40}}>Customize your {drink}</DialogContentText>
+          {/*<TextField*/}
+          {/*  autoFocus*/}
+          {/*  margin="dense"*/}
+          {/*  label="My new drink"*/}
+          {/*  type="email"*/}
+          {/*  fullWidth*/}
+          {/*  variant="standard"*/}
+          {/*  value={newDrink}*/}
+          {/*  onChange={(e) => setNewDrink(e.target.value)}*/}
+          {/*/>*/}
+            {ingredients.map((i) => {
+              return(
+                <FormControl style={{width: 400, paddingBottom:35}} >
+                  <InputLabel>{i}</InputLabel>
+                  <Select
+                      value={ingredient}
+                      label={i}
+                      onChange={handleChange}
+                  >
+                    <MenuItem value={1}>One</MenuItem>
+                    <MenuItem value={2}>Two</MenuItem>
+                    <MenuItem value={3}>Three</MenuItem>
+                  </Select>
+                </FormControl>
+              )})}
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleAddNewDrink}>Add New Drink</Button>
+          <Button onClick={handleAddToCart}>Add To Cart</Button>
         </DialogActions>
       </Dialog>
     </>
