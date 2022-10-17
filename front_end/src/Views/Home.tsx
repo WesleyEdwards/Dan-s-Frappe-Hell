4ea571181b9e471d82b027451a735d1f44eff308
LandonDoyle7599 <landondoyle7599@gmail.com>
2022-10-17 16:30:18 -0600

    Continue landing page work, hooked to backend

--- front_end/src/Views/Home.tsx
+++ front_end/src/Views/Home.tsx
@@ -1,23 +1,48 @@
 import {Container, Grid, Stack, Typography} from "@mui/material";
-import React, { FC, useState } from "react";
+import React, {FC, useEffect, useState} from "react";
 import { DFHeader } from "../components/DFHeader";
 import { DrinkCard } from "../components/DrinkCard";
+const backendUrl = "http://localhost:5000";
 
 export const Home: FC = () => {
-  const [drinks, setDrinks] = useState(["Frappe", "Cappuccino", "Latte", "Mocha", "Flat White", "Vanilla Bean"]);
+    useEffect(() => {
+        fetchData();
+    }, []);
 
-  const addDrink = (newDrink: string) => {
-    setDrinks([...drinks, newDrink]);
-  };
+    const [drinks, setDrinks] = useState(["Frappe", "Cappuccino", "Latte", "Mocha", "Flat White", "Vanilla Bean"]);
+
+    const [ingredients, setIngredients] = useState([""]);
+
+
+    async function fetchData() {
+
+        fetch(`${backendUrl}/ingredients/`, {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": ""
+            }
+        })
+            .then(r => r.json())
+            .then(json => {
+                const ingredientNames = [];
+                let result = json.ingredients;
+                console.log(result)
+                for (let i of result) {
+                    ingredientNames.push(i.Name);
+                }
+                setIngredients(result);
+            });
+    }
+
+
 
   return (
     <Container maxWidth="md">
-      <Typography>There are {drinks.length}</Typography>
       <Stack gap="2rem" justifyContent="center">
         <DFHeader title="Welcome to Dan's Frappuccino Hell" />
           <Grid container rowSpacing={4} columnSpacing={{md:8}}>
                 {drinks.map((drink) => {
-                    return <Grid item md={6}><DrinkCard drink={drink} description={"This is a description"} addDrink={addDrink}/></Grid>;
+                    return <Grid item md={6}><DrinkCard drink={drink} description={"This is a description"} ingredients={ingredients}/></Grid>;
                 })}
           </Grid>
       </Stack>
--- front_end/src/components/DrinkCard.tsx
+++ front_end/src/components/DrinkCard.tsx
@@ -10,81 +10,78 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import { FC, useState } from "react";
+import { FC, JSXElementConstructor, ReactElement, ReactFragment, ReactPortal, useState} from "react";
 
 interface DrinkCardProps {
   drink: string;
   description: string;
-  addDrink: (newDrink: string) => void;
+  ingredients: any;
 }
 
 export const DrinkCard: FC<DrinkCardProps> = (props) => {
-  const { drink, description, addDrink } = props;
+  const {drink, description, ingredients} = props;
 
   const [open, setOpen] = useState(false);
-  const [newDrink, setNewDrink] = useState("");
 
   const handleClick = () => {
     setOpen(true);
   };
   const handleClose = () => {
-    setNewDrink("");
     setOpen(false);
   };
 
-  const[ingredients, setIngredients] = useState(["Ingredient1", "Ingredient2", "Ingredient3","Ingredient4",
-  "Ingredient5", "Ingredient6", "Ingredient7", "Ingredient8","Ingredient9","Ingredient10","Ingredient11","Ingredient12"]);
-
   const handleAddToCart = () => {
     handleClose()
-    addDrink(newDrink);
   };
 
-    const [ingredient, setIngredient] = useState('');
-    const handleChange = (event: SelectChangeEvent) => {
-      setIngredient(event.target.value as string);
-    }
+  const [ingredient, setIngredient] = useState('1');
+  const handleChange = (event: SelectChangeEvent) => {
+    setIngredient(event.target.value as string);
+  }
 
   return (
-    <>
-      <Card
-        onClick={handleClick}
-        style={{
-          cursor: "pointer",
-          minWidth: "200px",
-          minHeight: "200px",
-        }}
-      >
-        <CardContent style={{justifyContent: "center"}}>
-          <Typography variant="h5" gutterBottom>{drink}</Typography>
-          <Typography variant="subtitle1" >{description}</Typography>
-        </CardContent>
-      </Card>
-      <Dialog open={open} onClose={handleClose}>
-        <DialogContent style={{width: 500}}>
-          <DialogContentText variant="h4" style={{paddingBottom: 40}}>Customize your {drink}</DialogContentText>
-          {/*<TextField*/}
-          {/*  autoFocus*/}
-          {/*  margin="dense"*/}
-          {/*  label="My new drink"*/}
-          {/*  type="email"*/}
-          {/*  fullWidth*/}
-          {/*  variant="standard"*/}
-          {/*  value={newDrink}*/}
-          {/*  onChange={(e) => setNewDrink(e.target.value)}*/}
-          {/*/>*/}
-            {ingredients.map((i) => {
+      <>
+        <Card
+            onClick={handleClick}
+            style={{
+              cursor: "pointer",
+              minWidth: "200px",
+              minHeight: "200px",
+            }}
+        >
+          <CardContent style={{justifyContent: "center"}}>
+            <Typography variant="h5" gutterBottom>{drink}</Typography>
+            <Typography variant="subtitle1">{description}</Typography>
+          </CardContent>
+        </Card>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogContent style={{width: 500}}>
+            <DialogContentText variant="h4" style={{paddingBottom: 40}}>Customize your {drink}</DialogContentText>
+            {ingredients.map((i: { Name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; }) => {
+              let v = "1"
+              let l = "1"
+              const handleChange = (event: SelectChangeEvent) => {
+                v = event.target.value as string;
+                l = event.target.value as string;
+              }
               return(
                 <FormControl style={{width: 400, paddingBottom:35}} >
-                  <InputLabel>{i}</InputLabel>
+                  <InputLabel style={{paddingBottom: 20}}>{i.Name}</InputLabel>
                   <Select
-                      value={ingredient}
-                      label={i}
+                      value={v}
+                      label={l}
                       onChange={handleChange}
                   >
-                    <MenuItem value={1}>One</MenuItem>
-                    <MenuItem value={2}>Two</MenuItem>
-                    <MenuItem value={3}>Three</MenuItem>
+                    <MenuItem value={1}>1</MenuItem>
+                    <MenuItem value={2}>2</MenuItem>
+                    <MenuItem value={3}>3</MenuItem>
+                    <MenuItem value={4}>4</MenuItem>
+                    <MenuItem value={5}>5</MenuItem>
+                    <MenuItem value={6}>6</MenuItem>
+                    <MenuItem value={7}>7</MenuItem>
+                    <MenuItem value={8}>8</MenuItem>
+                    <MenuItem value={9}>9</MenuItem>
+                    <MenuItem value={10}>10</MenuItem>
                   </Select>
                 </FormControl>
               )})}
