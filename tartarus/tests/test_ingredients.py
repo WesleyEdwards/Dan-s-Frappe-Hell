a92985ec6ed5f4d8b4cf7d117eb4bf1be54610b1
Joshua McClung <joshua.mcclung@aggiemail.usu.edu>
2022-10-19 15:15:28 -0600

    finished implementation/test/doc

--- tartarus/tests/test_ingredients.py
+++ tartarus/tests/test_ingredients.py
@@ -1,22 +1,5 @@
 import tartarus.ingredients
 
-def test_get_token_happy(client, app):
-    token_res = client.post(
-            '/auth/token',
-            json={'email':  'manager@dfh.com', 'password': 'password'}
-        )
-    assert token_res.status_code == 200
-    assert token_res.json['token'] != None
-
-def test_validate_token_happy(client, app):
-    token = client.post(
-            '/auth/token',
-            json={'email':  'manager@dfh.com', 'password': 'password'}
-        ).json['token']
-    print(token)
-    with app.app_context():
-        assert tartarus.auth.check_token(token)[1]
-
 def test_get_all(client,app,auth):
     token = auth.login()
     res = client.get('/ingredients/', headers={'Authorization':f'Bearer {token}'})
@@ -28,6 +11,7 @@ def test_get_id(client,auth):
     token = auth.login()
     res = client.get('/ingredients/1', headers={'Authorization':f'Bearer {token}'})
     assert res.status_code == 200
+    print(res.json['ingredient'])
     assert res.json['ingredient']['IngredientId'] == 1
 
 def test_get_types(client, auth):
